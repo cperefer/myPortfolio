c9efd854aafd3d7bd6f702de02e8aafd0d71f568
Miguel <migui.kaos14@gmail.com>
2025-10-12 12:32:34 +0200

    feat: created initial components

--- src/components/MyPortfolio.tsx
+++ src/components/MyPortfolio.tsx
@@ -1,9 +1,15 @@
+import { AboutMe } from "./AboutMe"
 import { Intro } from "./Intro"
+import { Portfolio } from "./Portfolio"
+import { Stack } from "./Stack"
 
 export const MyPortfolio = () => {
   return (
     <>
       <Intro />
+      <AboutMe />
+      <Stack />
+      <Portfolio />
     </>
   )
 }
---
+++ src/components/Stack.tsx
@@ -0,0 +1,5 @@
+export const Stack = () => {
+  return (
+    <section>Stack</section>
+  )
+}
