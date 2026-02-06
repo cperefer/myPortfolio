5fe153c5c833ed2fa834fb6a329ba8f4fe79250f
Miguel <migui.kaos14@gmail.com>
2026-02-06 17:25:39 +0100

    feat: changed style to center text correctly

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <section>
       <div className="container pb-[100px]">
         <MotionDiv>
-          <div className="w-[90vw] md:w-[60vw] text-center">
+          <div className="w-[90vw] text-center">
             <h3 className="text-2xl md:text-3xl pb-2 md:pb-5">¿Te convence lo que has visto?<br />Contacta conmigo!</h3>
             <p><a target="_blank" href='mailto:cmiguelperfer@gmail.com'>📩 Email</a> - <a target="_blank" href='https://www.linkedin.com/in/miguel-pérez-fernández-1571b6a6'>🏢 Linkedin</a> - <a target="_blank" href='https://github.com/cperefer'>🐈‍⬛ Github</a>
 
