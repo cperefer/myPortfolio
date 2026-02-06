8a2a9de35836e1ffa570ba5c91365d94d458b585
Miguel <migui.kaos14@gmail.com>
2026-02-06 17:38:20 +0100

    feat: added styles for ordering projects

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,23 +1,27 @@
-import { ProjectsDelivered } from "./data/ProjectsDelivered"
-import { MotionDiv } from "./shared/MotionDiv"
-import { PortfolioProject } from "./shared/PortfolioProject"
+import { ProjectsDelivered } from "./data/ProjectsDelivered";
+import { MotionDiv } from "./shared/MotionDiv";
+import { PortfolioProject } from "./shared/PortfolioProject";
 
 export const Portfolio = () => {
   return (
     <section>
       <div className="container pb-[100px] md:pb-[200px]">
         <MotionDiv>
-          <div className="w-[90vw] md:w-[60vw]">
-            <h3 className="text-2xl md:text-3xl md:pb-5">Proyectos destacados</h3>
-            {
-              ProjectsDelivered.map((project) => (
-                <PortfolioProject key={project.name} projectDelivered={project} />
-              ))
-            }
-
+          <div className="w-[90vw]">
+            <h3 className="text-2xl md:text-3xl md:pb-5">
+              Proyectos destacados
+            </h3>
+            <div className="flex row justify-between items-start">
+              {ProjectsDelivered.map((project) => (
+                <PortfolioProject
+                  key={project.name}
+                  projectDelivered={project}
+                />
+              ))}
+            </div>
           </div>
         </MotionDiv>
       </div>
     </section>
-  )
-}
+  );
+};
