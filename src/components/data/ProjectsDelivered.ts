4f4af9894f4f83a5cfa7d2386b46512d6c17481f
Miguel <migui.kaos14@gmail.com>
2026-02-15 20:30:54 +0100

    feat: added boxUp

--- src/components/data/ProjectsDelivered.ts
+++ src/components/data/ProjectsDelivered.ts
@@ -6,8 +6,13 @@ export interface ProjectDelivered {
 
 export const ProjectsDelivered: ProjectDelivered[] = [
   {
-    name: 'Valladolid Penguins',
-    url: 'https://valladolidpenguins.vercel.app',
-    stacks: ['NextJS', 'Tailwind CSS']
-  }
-] 
+    name: "Valladolid Penguins",
+    url: "https://valladolidpenguins.vercel.app",
+    stacks: ["NextJS", "Tailwind CSS"],
+  },
+  {
+    name: 'SaaS Gestión deportiva "BoxUp"',
+    url: "https://box-up-frontend.vercel.app/login?reference=portfolio",
+    stacks: ["ReactJS", "Typescript", "Tailwind CSS"],
+  },
+];
